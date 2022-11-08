e9d21623cc1aab2793360bc1be8135ab5ae41d09
Sarita <saritaradia65@gmail.com>
2022-11-08 12:02:40 +0000

    Added default profile picture in PostSchema model

--- api/models/post.js
+++ api/models/post.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   message: String,
   createdAt: Date,
+  profilePicture: {type: String, default: 'https://images.freeimages.com/vhq/images/previews/85b/psd-universal-blue-web-user-icon-392728.jpg'}
 });
 
 const Post = mongoose.model("Post", PostSchema);
