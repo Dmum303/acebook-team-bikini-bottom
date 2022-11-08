dccfbb29de61c24e88257b63a0615b58982e20ee
Sarita <saritaradia65@gmail.com>
2022-11-08 12:05:50 +0000

    Refactor

--- api/models/post.js
+++ api/models/post.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const PostSchema = new mongoose.Schema({
   message: String,
   createdAt: Date,
-  profilePicture: {type: String, default: 'https://images.freeimages.com/vhq/images/previews/85b/psd-universal-blue-web-user-icon-392728.jpg'}
+  profilePicture: {type: String, default: 'https://images.freeimages.com/vhq/images/previews/85b/psd-universal-blue-web-user-icon-392728.jpg'}, 
+  imageAddress: {type: String, default: ""}
 });
 
 const Post = mongoose.model("Post", PostSchema);
