3aeeee198f918332ad9acf9612142407f22fbeae
Dave T <105549233+Dmum303@users.noreply.github.com>
2022-11-09 12:06:12 +0000

    making comment showhide

--- frontend/src/components/post/Post.js
+++ frontend/src/components/post/Post.js
@@ -2,32 +2,38 @@ import React from 'react';
 import './Post.css';
 // import '../postComment/PostComment.js';
 import CommentForm from '../postCommentForm/CommentForm';
+import Comment from '../postComment/PostComment.js';
 
 // const arrNow = post.comment
 
 const Post = ({ post }) => {
+  const handleClick = () => {
+    console.log('Clicked');
+    var x = document.getElementById('hideComments');
+    if (x.style.display === 'none') {
+      x.style.display = 'block';
+    } else {
+      x.style.display = 'none';
+    }
+  };
   return (
     <div class="post-card">
       <article data-cy="post" key={post._id}>
         <br></br>
         {post.message}
-        {/* {post._id} */}
         <br></br>
-        <CommentForm postId={post._id} />
+        <button onClick={handleClick}>Comments</button>
         <br></br>
-        {console.log(post.comments.length)}
-        {post.comments
-          .slice(0)
-          .reverse()
-          .map((comment) => {
-            return (
-              <p>
-                {comment.text}
-                <br></br>
-                {comment.created}
-              </p>
-            );
-          })}
+        <div id="hideComments">
+          <CommentForm postId={post._id} />
+          <br></br>
+          {post.comments
+            .slice(0)
+            .reverse()
+            .map((comment) => (
+              <Comment comment={comment} key={post.id} />
+            ))}
+        </div>
       </article>
     </div>
   );
